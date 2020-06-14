80f2be9dad9286767745b55c79c1060d0b2784c6
Carson Skjerdal <Carson.skjerdal@Gmail.com>
2020-06-14 11:02:40 -0700

    init

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>TorchCMS Customer Coming Soon Page</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css">
+</head>
+<body>
+
+<style>
+a {
+    color: #0171BC;
+}
+</style>
+
+  <div class="container">
+
+	<div class="row" style="margin-top: 20%">
+  		<center><img src="hostuponlanding/website-icon.png" width="256" height="256"></center>
+  	</div>
+
+  	<div class="row" style="margin-top: 2%">
+        <h3 style="font-weight: bold; text-align: center;">Stay tuned, an awesome website is coming soon!</h3>
+        <p align="center">Reliable Web hosting for this website is provided by <a href="https://somethingelselabs.com">somethingelselabs.com</a>. Our client is still working on creating their amazing website. <br>
+          <br>
+        Check back again soon!</p>
+    </div>
+  
+
+  
+  	<div class="row" style="margin-top: 2%">
+        <p align="center"><a href="https://torchcms.com"><img src="hostuponlanding/torchcms-logo.png" alt="TorchCMS Logo"></a></p>
+    </div>
+
+
+  </div>
+
+</body>
+</html>
